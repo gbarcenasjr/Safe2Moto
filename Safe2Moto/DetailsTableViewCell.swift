531236fb1843142df9eccfb9062ae3bcebfb1bd5
Gerardo Barcenas Jr <gbarcenasjr@gmail.com>
2021-04-22 11:22:30 -0700

    UI Customizing Checkpoint 2

---
+++ Safe2Moto/DetailsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsTableViewCell.swift
+//  Safe2Moto
+//
+//  Created by Gerardo Barcenas Jr. on 4/22/21.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Safe2Moto/HomeViewController.swift
+++ Safe2Moto/HomeViewController.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var cityNameField: UITextField!
+    @IBOutlet weak var safetyRatingBanner: UIView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var safetyRatingLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        //cell.textLabel?.text = "row: \(indexPath.row)"
+        
+        return cell
+    }
+    
+    @IBAction func onSearchButton(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
