b8dbcc37686101336f5ea540af92ca381c879eec
Gerardo Barcenas Jr <gbarcenasjr@gmail.com>
2021-04-22 00:20:04 -0700

    Initial Commit

---
+++ Safe2Moto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe2Moto
+//
+//  Created by Gerardo Barcenas Jr. on 4/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
